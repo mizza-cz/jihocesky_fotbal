a16d58b0e95750457d76a1ef765e9a1e49811b30
mizza-cz <mizza.cz666@gmail.com>
2024-02-26 08:29:39 +0100

    fixes hp

--- src/components/topstory-section/topstory-section.js
+++ src/components/topstory-section/topstory-section.js
@@ -1,14 +1,8 @@
-$(document).ready(function () {
-  $(".topstory__article").mouseover(function () {
-    (idTopstoryCurrent = $(".topstory__content").data("topstory_current")),
-      (idTopstory = $(this).data("topstory")),
-      $(".topstory__article").removeClass("on"),
-      $(this).addClass("on"),
-      $("#topstory_content_" + idTopstoryCurrent).removeClass("on"),
-      $("#topstory_image_" + idTopstoryCurrent).removeClass("on"),
-      $("#topstory_content_" + idTopstory).addClass("on"),
-      $("#topstory_image_" + idTopstory).addClass("on"),
-      $(".topstory__content").data("topstory_current", idTopstory);
-  });
+$("#topstory").slick({
+  infinite: !0,
+  arrows: !1,
+  dots: !0,
+  autoplay: !1,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 });
-
