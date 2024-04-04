03759d22623ba788f77d34e618a94cc0a40a5349
mizza-cz <mizza.cz666@gmail.com>
2024-04-04 13:23:53 +0200

    add fixes

--- src/components/topstory-section/topstory-section.js
+++ src/components/topstory-section/topstory-section.js
@@ -6,3 +6,17 @@ $(".topstory").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 });
+
+$(".topstoryNews__slider").slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 3000,
+  autoplay: false,
+  prevArrow:
+    '<button class="slider__btn slider__btnprev"><img src="./images/ico/chevron-left-solid.svg" loading="lazy" alt="" /></button>',
+  nextArrow:
+    '<button class="slider__btn slider__btnnext"><img src="./images/ico/chevron-right-solid.svg" loading="lazy" alt="" /></button>',
+});
