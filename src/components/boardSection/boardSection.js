0ce201e45c7fe5ceca1ce8ab159003ba7b60526d
mizza-cz <mizza.cz666@gmail.com>
2024-05-10 12:33:53 +0200

    add gallery

--- src/components/boardSection/boardSection.js
+++ src/components/boardSection/boardSection.js
@@ -1,8 +1,8 @@
-$(".boardSection__slider").slick({
-  infinite: !0,
-  arrows: !1,
-  dots: !0,
-  autoplay: !1,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-});
+// $(".boardSection__slider").slick({
+//   infinite: !0,
+//   arrows: !1,
+//   dots: !0,
+//   autoplay: !1,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+// });
