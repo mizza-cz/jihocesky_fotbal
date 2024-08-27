992a6b0892fce65da4d4bff5ac4ad846ad369d02
mizza-cz <mizza.cz666@gmail.com>
2024-08-27 11:16:12 +0200

    add fixes

--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -2,9 +2,9 @@ $(".js-select").select2({
   width: "100%",
   minimumResultsForSearch: -1,
 });
-$(function () {
-  //presmerovani pomoci selectu
-  $(".js-select").change(function () {
-    location.href = $(this).val();
-  });
-});
+// $(function () {
+//   //presmerovani pomoci selectu
+//   $(".js-select").change(function () {
+//     location.href = $(this).val();
+//   });
+// });
